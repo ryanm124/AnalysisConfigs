abf85b5f934b89cf5d9278c0e1ad3f424e22e8b3
Ryan McCarthy <ryan.edward.mccarthy@cern.ch>
2023-05-16 19:53:21 +0200

    adding ttHbb dilepton config

---
+++ configs/ttHbb/custom_cut_functions.py
@@ -0,0 +1,39 @@
+import awkward as ak
+from pocket_coffea.lib.cut_definition import Cut
+
+def dilepton(events, params, year, sample, **kwargs):
+    MET = events[params["METbranch"][year]]
+    # Masks for same-flavor (SF) and opposite-sign (OS)
+    SF = ((events.nMuonGood == 2) & (events.nElectronGood == 0)) | (
+        (events.nMuonGood == 0) & (events.nElectronGood == 2)
+    )
+    OS = events.ll.charge == 0
+    # SFOS = SF & OS
+    not_SF = (events.nMuonGood == 1) & (events.nElectronGood == 1)
+    mask = (
+        (events.nLeptonGood == 2)
+        & (ak.firsts(events.LeptonGood.pt) > params["pt_leading_lepton"])
+        & (events.nJetGood >= params["njet"])
+        & (events.nBJetGood >= params["nbjet"])
+        & (MET.pt > params["met"])
+        & OS
+    )
+    # Pad None values with False
+    return ak.where(ak.is_none(mask), False, mask)
+
+dilepton_presel = Cut(
+    name="dilepton",
+    params={
+        "METbranch": {
+            '2016_PreVFP': "MET",
+            '2016_PostVFP': "MET",
+            '2017': "MET",
+            '2018': "MET",
+        },
+        "njet": 2,
+        "nbjet": 0,
+        "pt_leading_lepton": 15,
+        "met": 10,
+    },
+    function=dilepton,
+)
---
+++ configs/ttHbb/example_config.py
@@ -0,0 +1,145 @@
+from pocket_coffea.utils.configurator import Configurator
+from pocket_coffea.lib.cut_definition import Cut
+from pocket_coffea.lib.cut_functions import get_nObj_min, get_HLTsel, get_nBtagEq
+from pocket_coffea.parameters.cuts import passthrough
+from pocket_coffea.parameters.histograms import *
+from pocket_coffea.parameters.btag import btag_variations
+import workflow
+from workflow import ttHbbBaseProcessor
+
+import cloudpickle
+import custom_cut_functions 
+cloudpickle.register_pickle_by_value(workflow)
+cloudpickle.register_pickle_by_value(custom_cut_functions)
+
+from custom_cut_functions import *
+import os
+localdir = os.path.dirname(os.path.abspath(__file__))
+
+# Loading default parameters
+from pocket_coffea.parameters import defaults
+default_parameters = defaults.get_default_parameters()
+defaults.register_configuration_dir("config_dir", localdir+"/params")
+
+parameters = defaults.merge_parameters_from_files(default_parameters,
+                                                  f"{localdir}/params/object_preselection.yaml",
+                                                  f"{localdir}/params/triggers.yaml",
+                                                  update=True)
+
+
+cfg = Configurator(
+    parameters = parameters,
+    datasets = {
+        "jsons": [f"{localdir}/datasets/signal_ttHTobb_redirector.json"
+                    ],
+        "filter" : {
+            "samples": ["ttHTobb"],
+            "samples_exclude" : [],
+            "year": ['2018']
+        }
+    },
+
+    workflow = ttHbbBaseProcessor,
+    
+    skim = [get_nObj_min(1, 200., "FatJet"),
+            get_HLTsel(primaryDatasets=["DoubleEle","EleMu","DoubleMu"])], 
+    
+    preselections = [dilepton_presel],
+    categories = {
+        "baseline": [passthrough],
+        "1b" : [ get_nBtagEq(1, coll="BJetGood")],
+        "2b" : [ get_nBtagEq(2, coll="BJetGood")],
+        "3b" : [ get_nBtagEq(3, coll="BJetGood")],
+        "4b" : [ get_nBtagEq(4, coll="BJetGood")]
+    },
+
+    weights = {
+        "common": {
+            "inclusive": ["genWeight","lumi","XS",
+                          "pileup",
+                          "sf_ele_reco", "sf_ele_id",
+                          "sf_mu_id","sf_mu_iso",
+                          "sf_btag", "sf_jet_puId",
+                          ],
+            "bycategory" : {
+            }
+        },
+        "bysample": {
+        }
+    },
+
+    variations = {
+        "weights": {
+            "common": {
+                "inclusive": [  "pileup",
+                                "sf_ele_reco", "sf_ele_id",
+                                "sf_mu_id", "sf_mu_iso", "sf_jet_puId",
+                              ],
+                "bycategory" : {
+                }
+            },
+        "bysample": {
+        }    
+        },
+    },
+
+    
+   variables = {
+        **ele_hists(coll="ElectronGood", pos=0),
+        **muon_hists(coll="MuonGood", pos=0),
+        **count_hist(name="nElectronGood", coll="ElectronGood",bins=3, start=0, stop=3),
+        **count_hist(name="nMuonGood", coll="MuonGood",bins=3, start=0, stop=3),
+        **count_hist(name="nJets", coll="JetGood",bins=8, start=0, stop=8),
+        **count_hist(name="nBJets", coll="BJetGood",bins=8, start=0, stop=8),
+        **jet_hists(coll="JetGood", pos=0),
+        **jet_hists(coll="JetGood", pos=1),
+        **jet_hists(coll="JetGood", pos=2),
+        **jet_hists(coll="JetGood", pos=3),
+        **jet_hists(coll="JetGood", pos=4),
+        **jet_hists(name="bjet",coll="BJetGood", pos=0),
+        **jet_hists(name="bjet",coll="BJetGood", pos=1),
+        **jet_hists(name="bjet",coll="BJetGood", pos=2),
+        **fatjet_hists(name="fatjet",coll="FatJetGood"),
+
+       # 2D plots
+       "jet_eta_pt_leading": HistConf(
+           [
+               Axis(coll="JetGood", field="pt", pos=0, bins=40, start=0, stop=1000,
+                    label="Leading jet $p_T$"),
+               Axis(coll="JetGood", field="eta", pos=0, bins=40, start=-2.4, stop=2.4,
+                    label="Leading jet $\eta$"),
+           ]
+       ),
+       "jet_eta_pt_all": HistConf(
+           [
+               Axis(coll="JetGood", field="pt", bins=40, start=0, stop=1000,
+                    label="Leading jet $p_T$"),
+               Axis(coll="JetGood", field="eta", bins=40, start=-2.4, stop=2.4,
+                    label="Leading jet $\eta$")
+           ]
+       ),
+       
+
+    }
+)
+
+
+
+
+run_options = {
+        "executor"       : "dask/lxplus",
+        "env"            : "singularity",
+        "workers"        : 1,
+        "scaleout"       : 50,
+        "queue"          : "microcentury",
+        "walltime"       : "00:40:00",
+        "mem_per_worker" : "4GB", # GB
+        "disk_per_worker" : "1GB", # GB
+        "exclusive"      : False,
+        "chunk"          : 400000,
+        "retries"        : 50,
+        "treereduction"  : 20,
+        "adapt"          : False,
+        
+    }
+   
