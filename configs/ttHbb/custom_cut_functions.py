dcd03e85b2fbec1184524e52ffc18947b6482e96
Ashley <ash.marie.parker@gmail.com>
2023-10-31 16:26:03 +0100

    add Z mass window exclusion

--- configs/ttHbb/custom_cut_functions.py
+++ configs/ttHbb/custom_cut_functions.py
@@ -3,6 +3,9 @@ from pocket_coffea.lib.cut_definition import Cut
 
 def dilepton(events, params, year, sample, **kwargs):
     MET = events[params["METbranch"][year]]
+    # mass cut
+    min_mass = events.ll.mass > 20
+    mass_window = events.ll.mass > 106 or events.ll.mass < 76
     # Masks for same-flavor (SF) and opposite-sign (OS)
     SF = ((events.nMuonGood == 2) & (events.nElectronGood == 0)) | (
         (events.nMuonGood == 0) & (events.nElectronGood == 2)
@@ -17,6 +20,8 @@ def dilepton(events, params, year, sample, **kwargs):
         & (events.nBJetGood >= params["nbjet"])
         & (MET.pt > params["met"])
         & OS
+        & min_mass
+        & mass_window
     )
     # Pad None values with False
     return ak.where(ak.is_none(mask), False, mask)
