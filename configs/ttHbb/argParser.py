2a8fc8ef71d6a8767002e451ce361c8d170dd076
Ashley <ash.marie.parker@gmail.com>
2024-03-04 16:03:16 +0100

    change argParse to take a list

--- configs/ttHbb/argParser.py
+++ configs/ttHbb/argParser.py
@@ -9,7 +9,7 @@ def get_year_from_args():
     parser.add_argument("-ro", "--custom-run-options", type=str, default=None, help="User provided run options .yaml file")
     parser.add_argument("-o", "--outputdir", required=True, type=str, help="Output folder")
     parser.add_argument("-y", "--year", required=True, type=str, help="year")
-    parser.add_argument("-sa", "--sample", required=True, type=str, help="sample name")
+    parser.add_argument("-sa", "--sample", required=True, type=str, nargs='+', help="sample name")
     parser.add_argument("-t", "--test", action="store_true", help="Run with limit 1 interactively")
     parser.add_argument("-lf","--limit-files", type=int, help="Limit number of files")
     parser.add_argument("-lc","--limit-chunks", type=int, help="Limit number of chunks", default=None)
---
+++ configs/ttHbb/config_general_multiple.py
@@ -0,0 +1,256 @@
+from pocket_coffea.utils.configurator import Configurator
+from pocket_coffea.lib.cut_definition import Cut
+from pocket_coffea.lib.cut_functions import get_nObj_min, get_HLTsel,get_nBtagEq,get_nBtagMin, get_diLeptonFlavor
+from pocket_coffea.parameters.cuts import passthrough
+from pocket_coffea.parameters.histograms import *
+from pocket_coffea.lib.columns_manager import ColOut
+import os
+import workflow
+from workflow import ttHbbBaseProcessor 
+
+# importing custom cut functions
+import custom_cut_functions
+from  custom_cut_functions import *
+import cloudpickle
+cloudpickle.register_pickle_by_value(workflow)
+cloudpickle.register_pickle_by_value(custom_cut_functions)
+localdir = os.path.dirname(os.path.abspath(__file__))
+
+from argParser import get_year_from_args
+#import argparse
+import os
+from pocket_coffea.parameters import defaults
+
+#parser = argparse.ArgumentParser(description='Description of your script')
+#parser.add_argument('--year', type=int, help='Specify the year', required=True)
+#args = parser.parse_args()
+
+aargs = get_year_from_args()
+year = aargs[0] #'2018'#args.year
+samples = aargs[1]
+
+
+
+formatted_samples_files = []
+formatted_samples = []
+formatted_samples_names = []
+
+for i, s in enumerate(samples):
+    formatted_samples_files.append(f"{localdir}/datasets/{s}_{year}.json")
+    formatted_samples.append(f"formatted_samples_{i+1}")
+    formatted_samples_names.append(f"samples_{i+1}")
+
+# Loading default parameters
+default_parameters = defaults.get_default_parameters()
+defaults.register_configuration_dir("config_dir", localdir+"/params")
+
+# merging additional analysis specific parameters
+parameters = defaults.merge_parameters_from_files(default_parameters,
+                                                  f"{localdir}/params/object_preselection.yaml",
+                                                  f"{localdir}/params/triggers.yaml",
+                                                  update=True)
+
+# Configurator instance
+datasets_jsons = [f"{formatted_sample}" for formatted_sample in formatted_samples]
+datasets_filter_samples = [f"{formatted_sample_name}" for formatted_sample_name in formatted_samples_names]
+
+
+
+'''
+formatted_samples_1 = None
+formatted_samples_2 = None
+formatted_samples_3 = None
+formatted_samples_4 = None
+formatted_samples_5 = None
+print(samples)
+for i,s in samples:
+    if('_v7' in samples): s =s[:-3] # 'ZJetsToQQ_HT800toInf'
+    print(s)
+    if i == 0 :
+        formatted_samples_1 = "{localdir}/datasets/{s}_{year}.json"
+        samples_1 = s
+    if i == 1 :
+        formatted_samples_2 = "{localdir}/datasets/{s}_{year}.json"
+      	samples_2 = s
+        
+formatted_samples = "{localdir}/datasets/{sample}_{year}.json"
+
+#samples = aargs[1]
+#year = '2018'
+# Loading default parameters
+from pocket_coffea.parameters import defaults
+default_parameters = defaults.get_default_parameters()
+defaults.register_configuration_dir("config_dir", localdir+"/params")
+
+# merging additional analysis specific parameters
+parameters = defaults.merge_parameters_from_files(default_parameters,
+                                                  f"{localdir}/params/object_preselection.yaml",
+                                                  f"{localdir}/params/triggers.yaml",
+                                                  update=True)
+#sampleFilename = "DATA_SingleMuon_redirector.json"
+#sampleName = "SingleMuon"
+'''
+
+
+
+# Configurator instance
+#                  #f"{localdir}/datasets/DYJetsToLL_M-50_2018.json",#f"{localdir}/datasets/DATA_SingleMuon.json"                                                                               
+cfg = Configurator(
+    parameters = parameters,
+    datasets = {
+        "jsons": datasets_jsons ,
+        "filter" : {
+            "samples": datasets_filter_samples,
+            "samples_exclude" : [],
+            "year": [year]
+        } 
+    },
+
+    workflow = ttHbbBaseProcessor,
+    workflow_options = {"dump_columns_as_arrays_per_chunk": "root://eosuser.cern.ch//eos/user/r/rmccarth/ttHbb/chunks"},
+    
+    # Skimming and categorization
+    skim = [
+             get_nObj_min(1, 200., "FatJet"),
+             get_HLTsel(primaryDatasets=["DoubleEle","EleMu","DoubleMu"])
+             ],
+             
+    preselections = [dilepton_presel,
+                     get_nObj_min(2,25,"LeptonGood")],
+    
+    categories = {
+        "baseline": [passthrough],
+        "ee" : [get_diLeptonFlavor("ee")],
+        "emu" : [get_diLeptonFlavor("emu")],
+        "mumu" : [get_diLeptonFlavor("mumu")]
+    },
+    
+    # Weights configuration # "sf_ele_trigger", "sf_mu_trigger"
+    weights = {
+        "common": {
+            "inclusive": ["genWeight","lumi","XS",
+                          "pileup",
+                          "sf_ele_reco", "sf_ele_id",
+                          "sf_mu_id","sf_mu_iso",
+                          "sf_btag", "sf_jet_puId" 
+                          ],
+         
+        }
+    },
+
+    variations = {
+        "weights": {
+            "common": {
+                "inclusive": [  "pileup",
+                                "sf_ele_reco", "sf_ele_id",
+                                "sf_mu_id", "sf_mu_iso",
+                                 "sf_jet_puId","sf_btag"
+                                
+                              ]
+            },
+          
+        },
+       
+    },
+
+    
+    variables = {
+        **ele_hists(coll="ElectronGood", pos=0),
+        **ele_hists(coll="ElectronGood", pos=1),
+        **muon_hists(coll="MuonGood", pos=0),
+        **muon_hists(coll="MuonGood", pos=1),
+        **count_hist(name="nElectronGood", coll="ElectronGood",bins=3, start=0, stop=3),
+        **count_hist(name="nMuonGood", coll="MuonGood",bins=3, start=0, stop=3),
+        **count_hist(name="nJets", coll="JetGood",bins=8, start=0, stop=8),
+        **count_hist(name="nBJets", coll="BJetGood",bins=8, start=0, stop=8),
+        **count_hist(name="nBBFatJetGoodT", coll="BBFatJetGoodT",bins=3, start=0, stop=3),
+        **count_hist(name="nBBFatJetGoodM", coll="BBFatJetGoodM",bins=3, start=0, stop=3),
+        **count_hist(name="nBBFatJetGoodL", coll="BBFatJetGoodL",bins=3, start=0, stop=3),
+        **jet_hists(coll="JetGood", pos=0),
+        **jet_hists(coll="JetGood", pos=1),
+        **jet_hists(coll="JetGood", pos=2),
+        **jet_hists(coll="JetGood", pos=3),
+        **jet_hists(coll="JetGood", pos=4),
+        **jet_hists(name="bjet",coll="BJetGood", pos=0),
+        **jet_hists(name="bjet",coll="BJetGood", pos=1),
+        **jet_hists(name="bjet",coll="BJetGood", pos=2),
+        **fatjet_hists(name="fatjet",coll="FatJetGood"),
+        **fatjet_hists(name="bbfatjetTight",coll="BBFatJetGoodT"),
+        **fatjet_hists(name="bbfatjetMedium",coll="BBFatJetGoodM"),
+        **fatjet_hists(name="bbfatjetLoose",coll="BBFatJetGoodL"),
+
+       # 2D plots
+       "jet_eta_pt_leading": HistConf(
+           [
+               Axis(coll="JetGood", field="pt", pos=0, bins=40, start=0, stop=1000,
+                    label="Leading jet $p_T$"),
+               Axis(coll="JetGood", field="eta", pos=0, bins=40, start=-2.4, stop=2.4,
+                    label="Leading jet $\eta$"),
+           ]
+       ),
+       "jet_eta_pt_all": HistConf(
+           [
+               Axis(coll="JetGood", field="pt", bins=40, start=0, stop=1000,
+                    label="jet $p_T$"),
+               Axis(coll="JetGood", field="eta", bins=40, start=-2.4, stop=2.4,
+                    label="jet $\eta$")
+           ]
+       ),
+
+        "subleading_jetpt_MET" : HistConf(
+            [
+                Axis(coll="JetGood", field="pt", bins=40, start=0, stop=200, pos=1, label="Subleading jet $p_T$"),
+                Axis(coll="MET", field="pt", bins=40, start=0, stop=100, label="MET")
+            ] ),
+
+    },
+    
+    columns = {
+        "common": {
+            "inclusive": [
+                ColOut("JetGood", ["eta","pt","phi","btagDeepFlavB", "genJetIdx"]),
+                ColOut("FatJetGood", ["eta", "pt", "phi", "mass", "msoftdrop", "tau1", "tau2", "tau3", "tau4", "btagDDBvLV2", "deepTagMD_ZHbbvsQCD", "deepTagMD_ZHccvsQCD", "deepTagMD_HbbvsQCD", "deepTagMD_bbvsLight", "btagHbb", "genJetAK8Idx", "rhoQCD"]),
+                ColOut("LeptonGood",["eta","pt","phi","pdgId"]),
+                ColOut("MuonGood",["eta","pt","phi","jetRelIso","pfRelIso03_all", "miniPFRelIso_all" , "pfRelIso03_chg","miniPFRelIso_chg", "mvaTTH" ]),
+                ColOut("ElectronGood",["eta","pt","phi","jetRelIso", "pfRelIso03_all","miniPFRelIso_all", "pfRelIso03_chg","miniPFRelIso_chg", "mvaTTH" ]),                
+                ColOut("BJetGood", ["eta","pt","phi","btagDeepFlavB"]),
+                ColOut("BBFatJetGoodT", ["eta", "pt", "phi", "mass", "msoftdrop", "tau1", "tau2", "tau3", "tau4", "btagDDBvLV2", "deepTagMD_ZHbbvsQCD", "deepTagMD_ZHccvsQCD", "deepTagMD_HbbvsQCD", "deepTagMD_bbvsLight", "btagHbb", "rhoQCD"]),
+                ColOut("BBFatJetGoodM", ["eta", "pt", "phi", "mass", "msoftdrop", "tau1", "tau2", "tau3", "tau4", "btagDDBvLV2", "deepTagMD_ZHbbvsQCD", "deepTagMD_ZHccvsQCD", "deepTagMD_HbbvsQCD", "deepTagMD_bbvsLight", "btagHbb", "rhoQCD"]),
+                ColOut("BBFatJetGoodL", ["eta", "pt", "phi", "mass", "msoftdrop", "tau1", "tau2", "tau3", "tau4", "btagDDBvLV2", "deepTagMD_ZHbbvsQCD", "deepTagMD_ZHccvsQCD", "deepTagMD_HbbvsQCD", "deepTagMD_bbvsLight", "btagHbb", "rhoQCD"]),
+                ColOut("GenJet",["eta","hadronFlavour","mass","partonFlavour","phi","pt"]),
+                ColOut("GenFatJet",["eta","hadronFlavour","mass","partonFlavour","phi","pt"]),
+                ColOut("events",["genTtbarId"],store_size=False)
+            ]
+        }
+    }
+    
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+)
+'''
+run_options = {
+        "executor"       : "dask/lxplus",
+        "env"            : "singularity",
+        "workers"        : 1,
+        "scaleout"       : 50,
+        "worker_image"   : "/cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/cms-analysis/general/pocketcoffea:lxplus-cc7-latest",
+        "queue"          : "workday", # workday = 8 hour max job runtime : https://batchdocs.web.cern.ch/local/submit.html
+        "walltime"       : "00:40:00",
+        "mem_per_worker" : "4GB", # GB
+        "disk_per_worker" : "1GB", # GB
+        "exclusive"      : False,
+        "chunk"          : 400000,
+        "retries"        : 50,
+        "treereduction"  : 20,
+        "adapt"          : False,
+        "skipbadfiles"   : 10        
+    }
+'''
