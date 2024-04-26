abf01cac5fabd4454d79d358d02a8d2fb3a407f9
Ashley <ash.marie.parker@gmail.com>
2024-04-26 15:44:33 +0200

    adding semileptonic preselection

--- configs/ttHbb/custom_cut_functions.py
+++ configs/ttHbb/custom_cut_functions.py
@@ -55,3 +55,61 @@ dilepton_presel = Cut(
     },
     function=dilepton,
 )
+
+
+
+def boostedSemilep(events, params, year, sample, **kwargs):
+
+    MET = events[params["METbranch"][year]]
+    # mass cut
+
+    #dRWAK8 = 
+    min_mass =  (events.ll.mass > 20) & (events.ll.mass < 76 )
+    #print(min_mass)                                                                                                                                                                                                                          
+    mass_window = events.ll.mass > 106
+    #print(mass_window)                                                                                                                                                                                                                       
+    mass_cut = (min_mass) | (mass_window)
+
+    #Wmass_cut = 
+    #print(mass_cut)                                                                                                                                                                                                                          
+    # Masks for same-flavor (SF) and opposite-sign (OS)                                                                                                                                                                                       
+    SF = ((events.nMuonGood == 2) & (events.nElectronGood == 0)) | (
+        (events.nMuonGood == 0) & (events.nElectronGood == 2)
+    )
+    OS = events.ll.charge == 0
+    # SFOS = SF & OS                                                                                                                                                                                                                          
+    not_SF = (events.nMuonGood == 1) & (events.nElectronGood == 1)
+
+    mask = (
+        (events.nLeptonGood >= 1)
+        & (ak.firsts(events.ElectronGood.pt) > 30 )
+        & (ak.firsts(events.LeptonGood.pt) > params["pt_leading_lepton"])
+        & (events.nJetGood >= params["njet"])
+        & (events.nBJetGood >= params["nbjet"])
+        & (MET.pt > params["met"])
+        #& Wmass_cut
+        #& dRWAK8
+
+    )
+
+    return ak.where(ak.is_none(mask), False, mask)
+
+
+
+boostedSemilep_presel = Cut(
+    name="boostedSemilep",
+    params={
+        "METbranch": {
+            '2016_PreVFP': "MET",
+            '2016_PostVFP': "MET",
+            '2017': "MET",
+            '2018': "MET",
+        },
+        "njet": 2,
+        "nfatjet": 1,
+        "pt_leading_lepton": 26,
+        "met": 20,
+    },
+    function=boostedSemilep,
+)
+
